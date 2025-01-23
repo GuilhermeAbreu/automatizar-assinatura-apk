308ca23fa0b2bf8b9ea22f9e2d5347632abf9d57
Guilherme Abreu <Guilherme.abreu2019@gmail.com>
2025-01-23 09:59:24 -0300

    gerar o aab do play store
    gerar o aab do play store

--- deploy-app.js
+++ deploy-app.js
@@ -1,25 +1,21 @@
-// Importação de módulos
 require('dotenv').config();
 const shell = require('shelljs');
 const prompt = require('prompt-sync')();
 
-// Configuração das variáveis de ambiente
 const KEYSTORE_PASSWORD = process.env.KEYSTORE_PASSWORD || prompt('Digite a senha do keystore: ', { echo: '*' });
-const KEY_ALIAS = process.env.KEY_ALIAS || 'atendimento';
+const KEY_ALIAS = process.env.KEY_ALIAS || prompt('Digite a alias: ');
 const KEY_PASSWORD = process.env.KEY_PASSWORD || prompt('Digite a senha do alias: ', { echo: '*' });
 
-// Função principal para geração de build, sincronização, assinatura e otimização de APK
 function buildAndSignApks() {
     console.log('Iniciando processo de build e assinatura de APKs...');
-
-    // Limpa builds anteriores
     console.log('Limpando builds anteriores...');
     shell.rm('-rf', 'www');
     shell.rm('-rf', 'android/app/build');
     shell.rm('-rf', `${KEY_ALIAS}-release.apk`);
     shell.rm('-rf', `${KEY_ALIAS}-debug.apk`);
+    shell.rm('-rf', `${KEY_ALIAS}.aab`);
 
-    // Gera o build do Ionic + Angular para produção
+    shell.exec('npx capacitor-assets generate', { silent: true })
     console.log('Gerando build do Ionic + Angular para produção...');
     const ionicBuildResult = shell.exec('npx ionic build --prod', { silent: true });
     if (ionicBuildResult.code !== 0) {
@@ -28,7 +24,6 @@ function buildAndSignApks() {
     }
     console.log('Build do Ionic gerado com sucesso.');
 
-    // Sincroniza com Android Capacitor
     console.log('Sincronizando com Android Capacitor...');
     const syncResult = shell.exec('npx cap copy && npx cap sync', { silent: true });
     if (syncResult.code !== 0) {
@@ -36,15 +31,12 @@ function buildAndSignApks() {
         return;
     }
     console.log('Sincronização com Android Capacitor concluída.');
-
-    // Diretório do Android
     const androidDir = './android';
 
-    // Comandos para compilar APKs de release e debug
     const assembleReleaseCmd = `cd ${androidDir} && ./gradlew assembleRelease`;
     const assembleDebugCmd = `cd ${androidDir} && ./gradlew assembleDebug`;
+    const bundleReleaseCmd = `cd ${androidDir} && ./gradlew bundleRelease`;
 
-    // Gerando APK de release
     console.log('Gerando APK de release...');
     const assembleRelease = shell.exec(assembleReleaseCmd, { silent: false });
     if (assembleRelease.code !== 0) {
@@ -53,7 +45,6 @@ function buildAndSignApks() {
     }
     console.log('APK de release gerado com sucesso.');
 
-    // Gerando APK de debug
     console.log('Gerando APK de debug...');
     const assembleDebug = shell.exec(assembleDebugCmd, { silent: false });
     if (assembleDebug.code !== 0) {
@@ -62,14 +53,19 @@ function buildAndSignApks() {
     }
     console.log('APK de debug gerado com sucesso.');
 
-    // Caminhos e nomes dos APKs
+    console.log('Gerando AAB de debug...');
+    const bundleRelease = shell.exec(bundleReleaseCmd, { silent: false });
+    if (bundleRelease.code !== 0) {
+        console.error('Erro ao gerar AAB de debug:', bundleRelease.stderr);
+        return;
+    }
+
     const releaseApkPath = 'android/app/build/outputs/apk/release/app-release-unsigned.apk';
     const debugApkPath = 'android/app/build/outputs/apk/debug/app-debug.apk';
+    const releaseAabPath = 'android/app/build/outputs/bundle/release/app-release.aab'
 
-    // Caminho para o keystore
-    const keystorePath = `./${KEY_ALIAS}.jks`; // Substitua pelo seu caminho
+    const keystorePath = `./${KEY_ALIAS}.jks`;
 
-    // Assinando APK de release com apksigner
     console.log('Assinando APK de release...');
     const signReleaseApkCmd = `apksigner sign --ks ${keystorePath} --ks-key-alias ${KEY_ALIAS} --ks-pass pass:${KEYSTORE_PASSWORD} --key-pass pass:${KEY_PASSWORD} --out ${releaseApkPath.replace('-unsigned', '-signed.apk')} ${releaseApkPath}`;
     const signReleaseApk = shell.exec(signReleaseApkCmd, { silent: true });
@@ -79,7 +75,6 @@ function buildAndSignApks() {
     }
     console.log('APK de release assinado com sucesso.');
 
-    // Otimizando APK de release usando zipalign
     console.log('Otimizando APK de release...');
     const optimizedReleaseApk = `${releaseApkPath.replace('-unsigned', '-aligned.apk')}`;
     const zipalignCmd = `zipalign -v 4 ${releaseApkPath.replace('-unsigned', '-signed.apk')} ${optimizedReleaseApk}`;
@@ -90,13 +85,12 @@ function buildAndSignApks() {
     }
     console.log('APK de release otimizado com sucesso.');
 
-    // Movendo APKs para a raiz do projeto
     console.log('Movendo APKs para a raiz do projeto...');
     shell.mv(releaseApkPath.replace('-unsigned', '-signed.apk'), `${KEY_ALIAS}-release.apk`);
     shell.mv(debugApkPath, `${KEY_ALIAS}-debug.apk`);
+    shell.mv(releaseAabPath, `${KEY_ALIAS}.aab`);
 
     console.log('Build concluído com sucesso. APKs disponíveis na raiz do projeto.');
 }
 
-// Executando a função principal
 buildAndSignApks();
